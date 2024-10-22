d8fd00aa430648ee31117a496d598d1b924bc510
muskan060808 <muskan060808@gmail.com>
2024-10-22 19:53:24 +0530

    Add files via upload

---
+++ DAY20-CHEFGAMES.py
@@ -0,0 +1,8 @@
+# cook your dish here
+T=int(input( ))
+for i in range(T):
+    R1,R2,R3,R4=map(int,input().split())
+    if R1==0 and R2==0 and R3==0 and R4==0:
+        print("IN")
+    else:
+        print("OUT")
